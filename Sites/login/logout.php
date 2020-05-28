5a7c5bbc27002da99b7cec9fc89edae7274aa982
Sebastián Arriagada <snarriagada@uc.cl>
2020-05-28 11:35:33 -0400

    j

--- Sites/login/logout.php
+++ Sites/login/logout.php
@@ -4,8 +4,8 @@
 session_start();
 //Matamos la sesion
 session_destroy();
-echo 'Ha terminado la session <p><a href="index.php">index</a></p>';
+echo 'Ha terminado la session <p><a href="../index.php">index</a></p>';
 ?>
 <SCRIPT LANGUAGE="javascript">
-location.href = "index.php";
+location.href = "../index.php";
 </SCRIPT>
