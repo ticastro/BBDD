9a802f69abc0779b1a44e9ef38da6af7bd8c2574
ticastro <ticastro@uc.cl>
2020-05-28 17:39:59 -0400

    sesiones

--- Sites/login/logout.php
+++ Sites/login/logout.php
@@ -1,11 +1,11 @@
+<?php
+session_start();
+?>
+
 <?php include('../templates/header.html');   ?>
 
 <?php
-session_start();
 //Matamos la sesion
 session_destroy();
 echo 'Ha terminado la session <p><a href="../index.php">index</a></p>';
 ?>
-<SCRIPT LANGUAGE="javascript">
-location.href = "../index.php";
-</SCRIPT>
