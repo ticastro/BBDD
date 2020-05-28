9667e3bc9d6f4a568ba605e022f7db14c4fc266b
ticastro <ticastro@uc.cl>
2020-05-28 16:24:49 -0400

    afdg

--- Sites/login/validar_registro.php
+++ Sites/login/validar_registro.php
@@ -33,7 +33,9 @@ if ($usernames[0][0] == ""){
   $nuevo_uid = intval($uid[0][0]) + 1; 
   $nuevo_username = $_POST¨["username"];
   $nuevo_password = $_POST["password"];
-  echo "$nuevo_uid";
+  echo "$nuevo_uid\n";
+  echo "$nuevo_username\n";
+  echo "$nuevo_password";
   $query2 = "INSERT INTO borrador(uid,username,password) VALUES ('$nuevo_uid', '$nuevo_username', '$nuevo_password');" ;
   $result = $db -> prepare($query2);
   $result -> execute();
