574b5b49cb02de8f2547f4e796ce654b9ec7d29b
ticastro <ticastro@uc.cl>
2020-05-29 19:58:11 -0400

    asdf

--- Sites/paginas/pagina_lugar.php
+++ Sites/paginas/pagina_lugar.php
@@ -43,36 +43,47 @@ $query4 = "SELECT hr_apertura, hr_cierre from iglesia, lugar where lugar.nombre 
 
 
 <?php
-echo "Esta en la ciudad".$data1[0][0];
+echo "\n"."Esta en la ciudad".$data1[0][0];
 echo ", del país ".$data2[0][0];
 if ($data3 [0][0] != ""){
-echo $data3[0][0];
-echo $data3[0][1];
-echo $data3[0][2];
+echo "\n"."El museo abre a las ".$data3[0][0];
+echo "y cierra a las ".$data3[0][1];
+echo "con un precio de ".$data3[0][2];
 }else{
-echo $data4[0][0];
-echo $data4[0][1];
+echo "\n"."La iglesia abre a las ".$data4[0][0];
+echo "y cierra a las ".$data4[0][1];
 }
- 
+
+
+$query5 = "SELECT DISTINCT obras.nombre, obras.fecha_inicio, obras.fecha_culmino FROM obras, lugar WHERE obras.id_lugar = lugar.id_lugar AND lugar.nombre = '$nombre_lugar' ;" ;
+  $result = $db2 -> prepare($query5);
+  $result -> execute();
+  $data5 = $result -> fetchAll();
+
+
+$query6 = "SELECT DISTINCT artistas.nombre FROM obras, lugar,artistas WHERE obras.id_lugar = lugar.id_lugar AND lugar.nombre = '$nombre_lugar' AND artistas.id_artista = obras.id_artista ;"; 
+  $result = $db2 -> prepare($query6);
+  $result -> execute();
+  $data6 = $result -> fetchAll();
 ?>
 
 
  <table>
     <tr>
-      <th>Tus reservas</th>
+      <th>Obras en el lugar</th>
     </tr>
 
     <tr>
-      <th>hotel</th>
-      <th>check in</th>
-      <th>check out</th>
-      <th>direccion</th>
+      <th>Nombre</th>
+      <th>Año de inicio</th>
+      <th>Año de culmino</th>
+      <th>Link</th>
     </tr>
   
       <?php
-        foreach ($reservas as $p) {
+        foreach ($data5 as $p) {
 
-          echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td></tr>";
+          echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td><a href='pagina_obra.php?nombre='$p[0]'>Obra</a></td></tr>";
 
       }
       ?>
@@ -82,23 +93,19 @@ echo $data4[0][1];
 
   <table>
     <tr>
-      <th>Tus datos de viaje</th>
+      <th>Artistas con obra en este lugar</th>
     </tr>
 
     <tr>
-      <th>nº asiento</th>
-      <th>fecha compra</th>
-      <th>fecha viaje</th>
-      <th>hora salida</th>
-      <th>origen</th>
-      <th>destino</th>
+      <th>Nombre</th>
+      <th>Link</th>
     </tr>
   
   
       <?php
-        foreach ($datos as $p) {
-
-          echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td><td>$p[4]</td><td>$p[5]</td></tr>";
+        foreach ($data6 as $p) {
+          echo "<tr><td>$p[0]</td><td><a href='pagina_artista.php?nombre='$p[0]'>Artista</a></td></tr>";
+          
 
       }
       ?>
