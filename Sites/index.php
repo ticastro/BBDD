ea0a7976c6ddf812a80d30b45380458f3add51ed
Sebastián Arriagada <snarriagada@uc.cl>
2020-06-01 16:45:44 -0400

    j

--- Sites/index.php
+++ Sites/index.php
@@ -27,74 +27,8 @@ echo '<p><a href="login/sign_up.php">Registrarme</a></p>';
 
 <body>
   <h1 align="center"> Agencia de Viajes</h1>
-  <p style="text-align:center;">Aquí podrás encontrar información sobre los clientes y viajes de nuestra empresa.</p>
-
-  <br>
-
-  <h3 align="center"> Mostrar usernames y correos</h3>
-
-  <form align="center" action="consultas/consulta_uno.php" method="post">
-
-    <input type="submit" value="Mostrar">
-  </form>
-  
-  <br>
-  <br>
-  <br>
-
-  <h3 align="center"> Buscar ciudades por país</h3>
-
-  <form align="center" action="consultas/consulta_dos.php" method="post">
-    Nombre del país:
-    <input type="text" name="pais">
-    <br/><br/>
-    <input type="submit" value="Buscar">
-  </form>
-  
-  <br>
-  <br>
-  <br>
-
-  <h3 align="center"> Países en que ha hospedado un usuario: por username</h3>
-
-  <form align="center" action="consultas/consulta_tres.php" method="post">
-  
-    <input type="text" name="username">
-    <br/><br/>
-    <input type="submit" value="Buscar">
-  </form>
-  <br>
-  <br>
-  <br>
-
-  <h3 align="center"> Gastos por uid de usuario:</h3>
-
-  <form align="center" action="consultas/consulta_cuatro.php" method="post">
-  
-    <input type="text" name="uid">
-    <br/><br/>
-    <input type="submit" value="Buscar">
-  </form>
-  <br>
-  <br>
-  <br>
-
-  <h3 align="center"> Información entre 01/01/2020 al 31/03/2020</h3>
-
-  <form align="center" action="consultas/consulta_cinco.php" method="post">
-  
-    <br/><br/>
-    <input type="submit" value="Buscar">
-  </form>
-  <br>
-  <br>
-  <br>
+  <p style="text-align:center;">Ingresa para organizar tu próximo viaje.</p>
 
 
-  
-  <br>
-  <br>
-  <br>
-  <br>
 </body>
 </html>
