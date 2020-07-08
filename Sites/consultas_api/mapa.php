b17f656a6f7dcae700d26f9452ca2eea0c20a36f
ticastro <ticastro@uc.cl>
2020-07-08 19:02:19 -0400

    asdg

--- Sites/consultas_api/mapa.php
+++ Sites/consultas_api/mapa.php
@@ -32,7 +32,10 @@ $contador = 0;
 foreach ($datos as $d ) {
     $fecha = strval($d["date"]);
     $fecha_date = strtotime($fecha);
-    echo $fecha;
+    if ($fecha_inicial_date <= $fecha_date){
+    echo $fecha."<br />";    
+    }
+    
 
     if ($d["sender"] == $uid && $fecha_inicial <= $fecha_date && $fecha_final >= $fecha_date){
         $coordenadas = array();
