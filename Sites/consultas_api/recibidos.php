e58bb9d608347cb55c152329288f029019db9a2b
Sebastián Arriagada <snarriagada@uc.cl>
2020-07-07 20:03:01 -0400

    j

--- Sites/consultas_api/recibidos.php
+++ Sites/consultas_api/recibidos.php
@@ -3,15 +3,41 @@
 
 <body>
 <?php
+  $uid = $_SESSION["uid"];
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+
 
 
 
   $url = "https://lit-plateau-15934.herokuapp.com/messages";
   $json = file_get_contents($url);
   $datos = json_decode($json, True);
-  echo $datos ;
-  foreach($datos as $p){ echo $p["receptant"] ;}
+?>
+
+
+
+  <table>
+    <tr>
+      <th>Date</th>
+      <th>Lat</th>
+      <th>Long</th>
+      <th>mid</th>
+      <th>Receptant</th>
+      <th>Sender</th>
+      <th>Message</th>
+
+      
+    </tr>
+
+      <?php
+        foreach ($datos as $p) 
+          { if (intval($p["receptant"]) == $uid){
+          echo "<tr><td>$p["date"]</td><td>$p["lat"]</td><td>$p["long"]</td><td>$p["mid"]</td><td>$p["receptant"]</td><td>$p["sender"]</td><td>$p["message"]</td></tr>";
+        }
+      }
+      ?>
+      
+  </table>
 
 
 
